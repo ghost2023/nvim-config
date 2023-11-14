e0d579be3c6662ddeeddb51c9ca351b14b67477b
ghost2023 <nathnaelwonisha@gmail.com>
2023-11-14 19:14:36 +0300

    rm core/colorscheme

--- lua/ghost/core/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local status, _ = pcall(vim.cmd, "colorscheme nightfox")
-if not status then
-	print("Colorscheme not found!") -- print error if colorscheme not installed
-	return
-end
-
--- vim.cmd([[
---   autocmd InsertEnter * hi Cursor guifg=`green` guibg=`green`
--- ]])
--- lua/ghost/core/init.lua
+++ lua/ghost/core/init.lua
@@ -1,3 +1,2 @@
 require("ghost.core.keymaps")
 require("ghost.core.options")
-require("ghost.core.colorscheme")
