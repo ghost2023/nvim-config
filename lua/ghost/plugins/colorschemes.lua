4d67120ce1b0ab15bc0a6df2ef6f8464b2d06f30
ghost2023 <nathnaelwonisha@gmail.com>
2023-11-29 23:15:04 +0300

    changed colorscheme

--- lua/ghost/plugins/colorschemes.lua
+++ lua/ghost/plugins/colorschemes.lua
@@ -2,27 +2,34 @@ return {
 	{
 		"bluz71/vim-nightfly-guicolors",
 	},
-	{ "navarasu/onedark.nvim" },
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				style = "deep",
+			})
+		end,
+	},
 	{ "AlexvZyl/nordic.nvim" },
 	{ "sainnhe/sonokai" },
 	{ "Mofiqul/vscode.nvim" },
 	{ "Mofiqul/adwaita.nvim" },
 	{ "catppuccin/nvim", as = "catppuccin" },
 	{ "bluz71/vim-nightfly-guicolors" },
-  { "folke/tokyonight.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  {'jacoborus/tender.vim'},
-  {'EdenEast/nightfox.nvim'
-	  },
-  {'rose-pine/neovim'},
-  {'tiagovla/tokyodark.nvim',
-	priority = 1000, -- make sure to load this before all the other start plugins
+	{ "folke/tokyonight.nvim" },
+	{ "rebelot/kanagawa.nvim" },
+	{ "jacoborus/tender.vim" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rose-pine/neovim" },
+	{
+		"tiagovla/tokyodark.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
 			vim.cmd([[colorscheme tokyodark]])
 		end,
-  },
-  {'vigoux/oak'},
-  {'sainnhe/edge'},
-  {'ellisonleao/gruvbox.nvim'}
+	},
+	{ "vigoux/oak" },
+	{ "sainnhe/edge" },
+	{ "ellisonleao/gruvbox.nvim" },
 }
