ab7ce74d381895a3acc8ef8b2072dcdef1ebba05
ghost2023 <nathnaelwonisha@gmail.com>
2023-12-02 19:27:00 +0300

    random cmp tests

--- lua/ghost/plugins/nvim-cmp.lua
+++ lua/ghost/plugins/nvim-cmp.lua
@@ -11,6 +11,16 @@ return {
 	},
 	config = function()
 		local cmp = require("cmp")
+		local types = require("cmp.types")
+
+		local function deprioritize_snippet(entry1, entry2)
+			if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
+				return false
+			end
+			if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
+				return true
+			end
+		end
 
 		local luasnip = require("luasnip")
 
@@ -21,6 +31,25 @@ return {
 
 		-- 	completion = { completeopt = "menu,menuone,noinsert" },
 		cmp.setup({
+
+			-- sorting = {
+			-- 	priority_weight = 2,
+			-- 	comparators = {
+			-- 		deprioritize_snippet,
+			-- 		-- the rest of the comparators are pretty much the defaults
+			-- 		cmp.config.compare.offset,
+			-- 		cmp.config.compare.exact,
+			-- 		cmp.config.compare.scopes,
+			-- 		cmp.config.compare.score,
+			-- 		cmp.config.compare.recently_used,
+			-- 		cmp.config.compare.locality,
+			-- 		cmp.config.compare.kind,
+			-- 		cmp.config.compare.sort_text,
+			-- 		cmp.config.compare.length,
+			-- 		cmp.config.compare.order,
+			-- 	},
+			-- },
+
 			completion = {
 				completeopt = "menu,menuone,noinsert",
 			},
