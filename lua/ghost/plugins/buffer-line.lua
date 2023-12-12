120490ff0aafca0dadf29c26cf1d11e26593cfad
ghost2023 <nathnaelwonisha@gmail.com>
2023-12-12 09:55:32 +0300

    improvements in bufferline

--- lua/ghost/plugins/buffer-line.lua
+++ lua/ghost/plugins/buffer-line.lua
@@ -1,17 +1,27 @@
 return {
-'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require("bufferline").setup{
-      options = {
-        offsets = {{filetype = "NvimTree", text = "Files", highlight = "Directory", text_align = "left"}},
-        separator_style = "slant",
-        diagnostics = "nvim_lsp",
-        indicator = {
-          style = "underline",
-        },
-      }
-    }
-  end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				offsets = { { filetype = "NvimTree", text = "Files", highlight = "Directory", text_align = "left" } },
+				separator_style = "slant",
+				diagnostics = "nvim_lsp",
+				indicator = {
+					style = "underline",
+				},
+
+				diagnostics_update_in_insert = true,
+				show_buffer_icons = false,
+				hover = {
+					enabled = true,
+					delay = 200,
+					reveal = { "close" },
+				},
+			},
+		})
+	end,
 	-- "romgrk/barbar.nvim",
 	-- config = function()
 	-- 	require("barbar").setup({
