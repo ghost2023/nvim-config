762a0f5605bbf7fa03a17c92ff1207052ddbd62e
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-05 22:48:49 +0300

    improvement in buffer line

--- lua/ghost/plugins/buffer-line.lua
+++ lua/ghost/plugins/buffer-line.lua
@@ -3,9 +3,28 @@ return {
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		require("bufferline").setup({
+		local bufferline = require("bufferline")
+		vim.keymap.set("n", "<leader>bg", "<cmd>BufferLinePick<CR>") -- smart rename
+		vim.keymap.set("n", "<leader>bX", function()
+			for _, e in ipairs(bufferline.get_elements().elements) do
+				vim.schedule(function()
+					vim.cmd("bd " .. e.id)
+				end)
+			end
+		end)
+
+		bufferline.setup({
 			options = {
-				offsets = { { filetype = "NvimTree", text = "Files", highlight = "Directory", text_align = "left" } },
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "Files",
+						highlight = "Directory",
+						text_align = "left",
+						separator = true,
+					},
+				},
+				always_show_bufferline = false,
 				separator_style = "slant",
 				diagnostics = "nvim_lsp",
 				indicator = {
@@ -14,11 +33,6 @@ return {
 
 				diagnostics_update_in_insert = true,
 				show_buffer_icons = false,
-				hover = {
-					enabled = true,
-					delay = 200,
-					reveal = { "close" },
-				},
 			},
 		})
 	end,
