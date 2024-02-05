76f56b1814ec6a10acb5bca3e1219eb1cc9e28bb
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-05 22:51:15 +0300

    telescope improvement

--- lua/ghost/plugins/telescope.lua
+++ lua/ghost/plugins/telescope.lua
@@ -8,21 +8,35 @@ return {
 	},
 	config = function()
 		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 		local actions = require("telescope.actions")
 
 		telescope.setup({
+
 			defaults = {
-				path_display = { "truncate " },
+				path_display = { "tuncate" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["jk"] = actions.close,
 					},
 				},
 				file_ignore_patterns = {
 					"node_modules",
 					-- ".git",
+				},
+			},
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+					-- hidden = true,
+				},
+				mappings = {
+					i = {
+						["<c-x>"] = actions.delete_buffer + actions.move_to_top,
+					},
 				},
 			},
 		})
@@ -32,10 +46,23 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
+		local telescope_last = 0
+		function telescope_resume()
+			if telescope_last == 0 then
+				telescope_last = 1
+				builtin.live_grep()
+			else
+				builtin.resume()
+			end
+		end
+
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "Jumplist" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope registers<cr>", { desc = "registers" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fs", telescope_resume, { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = " string under cursor in cwd" })
 	end,
 }
 
