fa422b0c075b353edad804bd5ee73bd84962d0b6
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-05 22:48:34 +0300

    disable codeium

--- lua/ghost/plugins/codeium.lua
+++ lua/ghost/plugins/codeium.lua
@@ -1,8 +1,8 @@
 return {
-	"Exafunction/codeium.vim",
-	config = function()
-		vim.keymap.set("i", "<C-i>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true })
-	end,
+	-- "Exafunction/codeium.vim",
+	-- config = function()
+	-- 	vim.keymap.set("i", "<C-i>", function()
+	-- 		return vim.fn["codeium#Accept"]()
+	-- 	end, { expr = true })
+	-- end,
 }
