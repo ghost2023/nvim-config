c46538589a7aaea399f2878ee8f2bed909c1eba8
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-05 23:35:03 +0300

    setup scroll bar

--- lua/ghost/plugins/hlslens.lua
+++ lua/ghost/plugins/hlslens.lua
@@ -1,18 +1,17 @@
 return {
 	"kevinhwang91/nvim-hlslens",
 	config = function()
-		require("hlslens").setup()
+		require("hlslens").setup({
+			build_position_cb = function(plist, _, _, _)
+				require("scrollbar.handlers.search").handler.show(plist.start_pos)
+			end,
+		})
+
+		vim.cmd([[
+	       augroup scrollbar_search_hide
+	           autocmd!
+	           autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+	       augroup END
+	   ]])
 	end,
-	-- 		build_position_cb = function(plist, _, _, _)
-	-- 			require("scrollbar.handlers.search").handler.show(plist.start_pos)
-	-- 		end,
-	-- 	})
-	--
-	-- 	vim.cmd([[
-	--        augroup scrollbar_search_hide
-	--            autocmd!
-	--            autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
-	--        augroup END
-	--    ]])
-	-- end,
 }
