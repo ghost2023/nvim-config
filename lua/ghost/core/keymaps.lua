433cff3c8c4d31b47507d648e91961367719de84
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-24 21:19:45 +0300

    keymaps

--- lua/ghost/core/keymaps.lua
+++ lua/ghost/core/keymaps.lua
@@ -2,13 +2,14 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+local opts = { noremap = true, silent = true }
 
 ---------------------
 -- General Keymaps
 ---------------------
 
 -- use jk to exit insert mode
--- keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jk", "<ESC>")
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>")
@@ -18,10 +19,6 @@ keymap.set("n", "x", '"_x')
 
 keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>") -- see available code actions
 keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>") -- smart rename
-
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>") -- increment
-keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
@@ -33,6 +30,9 @@ keymap.set("n", "<leader>bn", ":bnext<CR>") -- go to next buffer
 keymap.set("n", "<leader>bp", ":bprevious<CR>") -- go to previous buffer
 keymap.set("n", "<leader>bb", ":b#<CR>") -- go to previous buffer
 keymap.set("n", "<leader>bx", ":bdelete<CR>") -- close buffer
+
+keymap.set("n", "<leader>k", ":bprevious<CR>") -- go to previous buffer
+keymap.set("n", "<leader>j", ":bnext<CR>") -- go to next buffer
 
 -- windows keybinding
 keymap.set("n", "<leader>ww", "<C-w>w")
@@ -53,10 +53,6 @@ keymap.set("n", "<leader>wx", "<C-w>q")
 keymap.set("n", "<leader>wv", "<C-w>v")
 keymap.set("n", "<leader>ws", "<C-w>s")
 
--- window maximization
-keymap.set("n", "<leader>wmh", "<C-w>_")
-keymap.set("n", "<leader>wmw", "<C-w>|")
-
 ----------------------
 -- Plugin Keybinds
 ----------------------
@@ -69,13 +65,6 @@ keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files wit
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-
--- telescope git commands (not on youtube nvim video)
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
@@ -85,11 +74,8 @@ keymap.set("n", "<C-q>", ":qa<CR>")
 keymap.set("i", "<C-q>", "<cmd>qa<CR>")
 
 -- save
--- keymap.set("i", "/ss", "<ESC><cmd>w<CR>")
-keymap.set("n", "<leader>ss", "<cmd>w<CR>")
-keymap.set("v", "<leader>ss", "<cmd>w<CR>")
+keymap.set("n", "<leader>t", "<cmd>w<CR>")
 keymap.set("n", "<C-s>", "<ESC><cmd>w<CR>")
-keymap.set("v", "<C-s>", "<ESC><cmd>w<CR>")
 keymap.set("i", "<C-s>", "<ESC><cmd>w<CR>")
 
 -- toggle terminal
@@ -97,22 +83,49 @@ keymap.set("n", "<f8>", ":ToggleTerm<CR>")
 keymap.set("i", "<f8>", "<ESC><cmd>ToggleTerm<CR>")
 keymap.set("t", "<f8>", "<cmd>ToggleTerm<CR>")
 
--- control backspace
--- keymap.set("i", "<C-BS>", "<C-w>", { noremap = false, silent = true })
+-- set keybinds
+opts.desc = "Show LSP references"
+keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
--- vim.api.nvim_set_keymap('i', '<C-BS>', '<C-w>', { noremap = true, silent = true })
+opts.desc = "Go to declaration"
+keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
--- Enable Ctrl + Backspace to delete a word in insert mode
--- vim.api.nvim_set_keymap('i', '<C-W>', '<C-w>', { noremap = true, silent = true })
+opts.desc = "Show LSP definitions"
+keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts) -- show lsp definitions
 
--- -- git
--- keymap.set("n", "gd", "<cmd>DiffviewOpen<CR>")
--- keymap.set("n", "gv", "<cmd>Git<CR>")
--- keymap.set("n", "gp", "<cmd>:Git push<CR>")
+opts.desc = "Show LSP implementations"
+keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
--- trouble
-keymap.set("n", "<f7>", "<cmd>TroubleToggle<CR>")
-keymap.set("i", "<f7>", "<cmd>TroubleToggle<CR>")
+opts.desc = "Show LSP type definitions"
+keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-keymap.set("i", "jk", "<ESC>")
--- keymap.set("v", "jk", "<ESC>")
+opts.desc = "See available code actions"
+keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+opts.desc = "Smart rename"
+keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+opts.desc = "Show buffer diagnostics"
+keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+opts.desc = "Show line diagnostics"
+keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+opts.desc = "Go to previous diagnostic"
+keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+opts.desc = "Go to next diagnostic"
+keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+opts.desc = "Show documentation for what is under cursor"
+keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+
+opts.desc = "Restart LSP"
+keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+keymap.set("n", "<leader>oi", function()
+	vim.lsp.buf.execute_command({
+		command = "_typescript.organizeImports",
+		arguments = { vim.fn.expand("%:p") },
+	})
+end, opts)
