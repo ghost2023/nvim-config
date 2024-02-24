722f042c1e7c3e5512e18e0c6c5ec5b3ba1de3eb
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-24 20:27:25 +0300

    autosession improvements

--- lua/ghost/plugins/autosession.lua
+++ lua/ghost/plugins/autosession.lua
@@ -5,13 +5,24 @@ return {
 		local auto_session = require("auto-session")
 
 		auto_session.setup({
-			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+			session_lens = {
+				buftypes_to_ignore = {},
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = false,
+			},
 		})
 
 		local keymap = vim.keymap
 
 		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
 		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+		keymap.set(
+			"n",
+			"<leader>wl",
+			require("auto-session.session-lens").search_session,
+			{ desc = "Save session for auto session root dir" }
+		) -- save workspace session for current working directory
 	end,
 }
