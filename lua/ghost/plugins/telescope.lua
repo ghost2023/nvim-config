78003b750b34cb6bec6a83f0d223e9578f000961
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-29 19:16:28 +0300

    rm dead code

--- lua/ghost/plugins/telescope.lua
+++ lua/ghost/plugins/telescope.lua
@@ -46,16 +46,6 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		local telescope_last = 0
-		function telescope_resume()
-			if telescope_last == 0 then
-				telescope_last = 1
-				builtin.live_grep()
-			else
-				builtin.resume()
-			end
-		end
-
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fj", "<cmd>Telescope jumplist<cr>", { desc = "Jumplist" })
 		keymap.set("n", "<leader>fg", "<cmd>Telescope registers<cr>", { desc = "registers" })
@@ -65,41 +55,3 @@ return {
 		keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = " string under cursor in cwd" })
 	end,
 }
-
--- import telescope plugin safely
--- local telescope_setup, telescope = pcall(require, "telescope")
--- if not telescope_setup then
--- 	return
--- end
---
--- -- import telescope actions safely
--- local actions_setup, actions = pcall(require, "telescope.actions")
--- if not actions_setup then
--- 	return
--- end
---
--- -- configure telescope
--- telescope.setup({
--- 	-- configure custom mappings
--- 	defaults = {
--- 		mappings = {
--- 			i = {
--- 				["<C-k>"] = actions.move_selection_previous, -- move to prev result
--- 				["<C-j>"] = actions.move_selection_next, -- move to next result
--- 				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
--- 			},
--- 		},
--- 		file_ignore_patterns = {
--- 			"node_modules",
--- 			-- ".git",
--- 		},
--- 	},
--- 	pickers = {
--- 		find_files = {
--- 			find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
--- 			-- hidden = true,
--- 		},
--- 	},
--- })
---
--- telescope.load_extension("fzf")
