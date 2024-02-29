b269ce30b398370fe558409021bcb3243c307147
ghost2023 <nathnaelwonisha@gmail.com>
2024-02-29 19:18:26 +0300

    rm maximize

--- lua/ghost/core/keymaps.lua
+++ lua/ghost/core/keymaps.lua
@@ -57,9 +57,6 @@ keymap.set("n", "<leader>ws", "<C-w>s")
 -- Plugin Keybinds
 ----------------------
 
--- vim-maximizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
-
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
