b02d52828ad7301c56688f15b85922ec52486edc
ghost2023 <nathnaelwonisha@gmail.com>
2024-04-06 15:33:55 +0300

    add keymaps

--- lua/ghost/core/keymaps.lua
+++ lua/ghost/core/keymaps.lua
@@ -57,11 +57,16 @@ keymap.set("n", "<leader>ws", "<C-w>s")
 -- undotree
 keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>")
 
+-- git
+keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<cr>")
+keymap.set("n", "<leader>gb", "<cmd>GitBlameToggle<cr>")
+
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fe", "<cmd>Telescope resume<cr>") -- resume telescope
 
 -- restart lsp server (not on youtube nvim video)
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
