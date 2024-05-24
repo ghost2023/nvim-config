79033ca5a235c2657c4fde546860f88be70a2b91
ghost2023 <nathnaelwonisha@gmail.com>
2024-05-24 11:11:38 +0300

    twoslash query

--- lua/ghost/plugins/ts.lua
+++ lua/ghost/plugins/ts.lua
@@ -1,6 +1,20 @@
 return {
-	"dmmulroy/tsc.nvim",
-	config = function()
-		require("tsc").setup()
-	end,
+	{
+		"marilari88/twoslash-queries.nvim",
+		config = function()
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			require("lspconfig")["tsserver"].setup({
+				capabilities = cmp_nvim_lsp.default_capabilities(),
+				on_attach = function(client, bufnr)
+					require("twoslash-queries").attach(client, bufnr)
+				end,
+			})
+		end,
+	},
+	{
+		"dmmulroy/tsc.nvim",
+		config = function()
+			require("tsc").setup()
+		end,
+	},
 }
