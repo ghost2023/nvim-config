1d5563814df0e95240f8a59042532d1f54c234a0
ghost2023 <nathnaelwonisha@gmail.com>
2024-06-16 17:59:49 +0300

    clean up color schemes

--- lua/ghost/plugins/colorschemes.lua
+++ lua/ghost/plugins/colorschemes.lua
@@ -1,16 +1,7 @@
 return {
 	{
-		"navarasu/onedark.nvim",
-		config = function()
-			require("onedark").setup({
-				style = "deep",
-			})
-		end,
-	},
-	{
 		"w0ng/vim-hybrid",
 	},
-	{ "rockyzhang24/arctic.nvim" },
 	{
 		"marko-cerovac/material.nvim",
 		config = function()
@@ -18,18 +9,12 @@ return {
 		end,
 		priority = 1000, -- make sure to load this before all the other start plugins
 	},
-	{ "sainnhe/edge" },
-	{ "ray-x/starry.nvim" },
 	{ "Mofiqul/vscode.nvim" },
-	{ "rafalbromirski/vim-aurora" },
 	{ "catppuccin/nvim", as = "catppuccin" },
 	{ "nanotech/jellybeans.vim" },
 	{ "folke/tokyonight.nvim" },
-	{ "rebelot/kanagawa.nvim" },
 	{ "EdenEast/nightfox.nvim" },
 	{ "rose-pine/neovim" },
-	{
-		"tiagovla/tokyodark.nvim",
-	},
+	{ "tiagovla/tokyodark.nvim", },
 	{ "projekt0n/github-nvim-theme" },
 }
