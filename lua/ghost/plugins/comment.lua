77a285b17c09deed4e84e151e5ce0c0e088a271c
ghost2023 <nathnaelwonisha@gmail.com>
2024-06-16 18:00:12 +0300

    clean some code

--- lua/ghost/plugins/comment.lua
+++ lua/ghost/plugins/comment.lua
@@ -1,19 +1,10 @@
 return {
 	"numToStr/Comment.nvim",
+	event = "VeryLazy",
 	config = function()
 		local comment = require("Comment")
-
 		comment.setup({
 			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 		})
 	end,
 }
-
--- -- import comment plugin safely
--- local setup, comment = pcall(require, "Comment")
--- if not setup then
---   return
--- end
---
--- -- enable comment
--- comment.setup()
