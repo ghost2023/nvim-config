9901d1116b5f3f569d83a5d99eb8c930e4b7199d
ghost2023 <nathnaelwonisha@gmail.com>
2024-09-01 21:09:04 +0300

    svelte lsp update

--- lua/ghost/plugins/lsp/lspconfig.lua
+++ lua/ghost/plugins/lsp/lspconfig.lua
@@ -51,13 +51,27 @@ return {
 		lspconfig["svelte"].setup({
 			capabilities = capabilities,
 			on_attach = function(client, bufnr)
-				vim.api.nvim_create_autocmd("BufWritePost", {
+				vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "TextChangedP" }, {
 					pattern = { "*.js", "*.ts" },
 					callback = function(ctx)
 						if client.name == "svelte" then
-							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+							client.notify(
+								"$/onDidChangeTsOrJsFile",
+								{
+									uri = ctx.file,
+									changes = {
+										{
+											text = table.concat(
+												vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false),
+												"\n"
+											),
+										},
+									},
+								}
+							)
 						end
 					end,
+					group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
 				})
 			end,
 		})
