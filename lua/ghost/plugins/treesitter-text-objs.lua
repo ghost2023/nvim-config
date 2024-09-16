3928ce750d9b3c65efdef57070e0ed978af9178d
ghost2023 <nathnaelwonisha@gmail.com>
2024-09-16 18:02:49 +0300

    rm text-objs

--- lua/ghost/plugins/treesitter-text-objs.lua
+++ lua/ghost/plugins/treesitter-text-objs.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  event = "VeryLazy",
   config = function()
     require("nvim-treesitter.configs").setup({
       textobjects = {
@@ -46,17 +47,7 @@ return {
           },
         },
         swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>na"] = "@parameter.inner", -- swap parameters/argument with next
-            ["<leader>n:"] = "@property.outer", -- swap object property with next
-            ["<leader>nm"] = "@function.outer", -- swap function with next
-          },
-          swap_previous = {
-            ["<leader>pa"] = "@parameter.inner", -- swap parameters/argument with prev
-            ["<leader>p:"] = "@property.outer", -- swap object property with prev
-            ["<leader>pm"] = "@function.outer", -- swap function with previous
-          },
+          enable = false,
         },
         move = {
           enable = true,
