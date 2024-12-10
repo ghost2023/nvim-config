a7e4805e2a76c4ec8669053df40bc1f2d2fd911d
ghost2023 <nathnaelwonisha@gmail.com>
2024-12-10 22:20:42 +0300

    options

--- lua/ghost/core/options.lua
+++ lua/ghost/core/options.lua
@@ -15,8 +15,13 @@ vim.opt.fillchars = { eob = " " }
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
+
+opt.backup = false
 opt.backupcopy = "yes"
-opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 -- tabs & indentation
 opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
@@ -42,7 +47,7 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
-vim.opt.updatetime = 250
+vim.opt.updatetime = 450
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -75,3 +80,19 @@ opt.guicursor = {
   "o:hor25",
   "a:blinkwait700-blinkoff400-blinkon250",
 }
+
+local function check_if_modified()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local original_content = vim.fn.systemlist("cat " .. vim.fn.expand("%"))
+  local current_content = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
+
+  if vim.deep_equal(original_content, current_content) then
+    vim.api.nvim_set_option_value("modified", false, { buf = 0 })
+  end
+end
+
+-- Run the check on every BufWritePost and TextChanged event
+vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged" }, {
+  pattern = "*",
+  callback = check_if_modified,
+})
