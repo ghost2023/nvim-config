df55e23c29d3077a6105fc685b6eed773d2260d4
ghost2023 <nathnaelwonisha@gmail.com>
2024-12-10 22:21:20 +0300

    update lazy

--- lua/ghost/lazy.lua
+++ lua/ghost/lazy.lua
@@ -1,25 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "ghost.plugins" }, { import = "ghost.plugins.lsp" } }, {
-	install = {
-		colorscheme = { "carbonfox" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
+  install = {
+    colorscheme = { "hybrid" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
